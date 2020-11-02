89ef7ec2af0791deba80c4369dcd249cdf54de45
AidanDennehy <44627373+Dracfo@users.noreply.github.com>
2020-11-02 08:12:13 -0500

    Working markdown to html and entry page loading

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,4 @@
+from markdown2 import Markdown
 from django.shortcuts import render
 
 from . import util
@@ -8,3 +9,29 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+# Get and return the desired wiki entry
+def wiki(request, title):  
+    # Get the entry from the url
+    entry = util.get_entry(title)
+
+    # Check if the entry exists
+    if not entry:
+        return render(request, "encyclopedia/entry.html", {
+            "entry": "ERROR: Requested File Not Found",
+            "title": "ERROR"
+        })
+
+    # Convert the markdown entry to html
+    markdowner = Markdown()
+    html = markdowner.convert(entry)
+
+    # Else return the requested page
+    return render(request, "encyclopedia/entry.html", {
+        "entry": html,
+        "title": title
+    })
+
+
+def search(request):
+    return render(request, "encyclopedia/search.html")
